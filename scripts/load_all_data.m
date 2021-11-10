d32edba4b8d027fb1480941923003baea56cef04
lcossell <l.cossell@ucl.ac.uk>
2021-11-10 16:07:15 +0000

    minor mods

--- scripts/load_all_data.m
+++ scripts/load_all_data.m
@@ -1,10 +1,9 @@
 % takes about 4 minutes to load all the data waveforms
-ctl = RC2Analysis();
-probe_ids = ctl.get_probe_ids('visual_flow', 'darkness', 'mismatch_nov20', 'mismatch_jul21');
+ctl         = RC2Analysis();
+probe_ids   = ctl.get_probe_ids('visual_flow', 'darkness', 'mismatch_nov20', 'mismatch_jul21', 'mismatch_darkness_oct21');
 
 for ii = 1 : length(probe_ids)
     
     fprintf('%i/%i\n', ii, length(probe_ids));
-        
     data(ii) = ctl.load_formatted_data(probe_ids{ii});
 end
--- scripts/preprocess/create_tables.m
+++ scripts/preprocess/create_tables.m
@@ -1,11 +1,11 @@
-experiment_groups    = {'visual_flow', 'darkness', 'mismatch_nov20', 'mismatch_jul21', 'mismatch_darkness_oct21'};
+experiment_groups    = {'visual_flow', 'darkness'};%, 'mismatch_nov20', 'mismatch_jul21', 'mismatch_darkness_oct21'};
 
 ctl                  = RC2Analysis();
 probe_ids            = ctl.get_probe_ids(experiment_groups{:});
 
-for ii = 2 : length(probe_ids)
+for ii = 1 : length(probe_ids)
     ii
     % load formatted data
-    ctl.create_replay_offsets_table(probe_ids{ii});
+%     ctl.create_replay_offsets_table(probe_ids{ii});
     ctl.create_svm_table(probe_ids{ii});
 end
