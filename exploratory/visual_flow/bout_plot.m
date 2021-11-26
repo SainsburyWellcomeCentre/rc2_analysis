9f1252f20d51f41e5e7a7bba05c237cf6ba65f7a
lcossell <l.cossell@ucl.ac.uk>
2021-11-26 11:11:19 +0000

    remove old unused files and functions

--- exploratory/visual_flow/bout_plot.m
+++
@@ -1,8 +0,0 @@
-function bout_plot(h_ax, base_t, trace, bout)
-
-pad = 5000;
-hold on;
-idx = (bout.start_idx-pad):(bout.end_idx+pad);
-plot(base_t(idx)-base_t(bout.start_idx), trace(idx), 'color', 'k');
-set(h_ax, 'plotboxaspectratio', [3, 1, 1]);
-box off;
