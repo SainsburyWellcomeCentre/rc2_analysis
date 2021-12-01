5cdfa6a701d297c7f3d5de9bbf780a8ac5e8c334
lcossell <l.cossell@ucl.ac.uk>
2021-12-01 18:43:58 +0000

    save git information about runningmouse and rc2_analysis

--- lib/classes/preprocess/CameraProcessingHelper.m
+++ lib/classes/preprocess/CameraProcessingHelper.m
@@ -4,6 +4,9 @@ classdef CameraProcessingHelper < handle
         
         ctl
         session_id
+        
+        local_dir
+        camera_ids
     end
     
     
@@ -18,11 +21,24 @@ classdef CameraProcessingHelper < handle
         
         
         
+        function val = get.local_dir(obj)
+            val = obj.ctl.file.camera_csv_dir_fast(obj.session_id);
+        end
+        
+        
+        
+        function val = get.camera_ids(obj)
+            val = obj.ctl.get_camera_ids(obj.session_id);
+        end
+        
+        
+        
         function run_from_raw(obj)
             
             obj.move_raw_to_local();
             obj.run_runningmouse();
             obj.transform_avi();
+            obj.save_git();
         end
         
         
@@ -30,10 +46,9 @@ classdef CameraProcessingHelper < handle
         function move_raw_to_local(obj)
             
             remote_dir = obj.ctl.file.raw_camera_dir(obj.session_id);
-            local_dir = obj.ctl.file.camera_csv_dir_fast(obj.session_id);
             
             % copy the files from remote to local
-            cmd = sprintf('xcopy /E /D "%s" "%s"', remote_dir, [local_dir, '\']);
+            cmd = sprintf('xcopy /E /D "%s" "%s"', remote_dir, [obj.local_dir, '\']);
             system(cmd);
         end
         
@@ -41,21 +56,20 @@ classdef CameraProcessingHelper < handle
         
         function run_runningmouse(obj)
             
-            local_dir = obj.ctl.file.camera_csv_dir_fast(obj.session_id);
-            camera_ids = obj.ctl.get_camera_ids(obj.session_id);
-            
             % run the python pipeline
-            for ii = 1 : length(camera_ids)
-                camera_fname = fullfile(local_dir, sprintf('%s.avi', camera_ids{ii}));
+            for ii = 1 : length(obj.camera_ids)
+                camera_fname = fullfile(obj.local_dir, sprintf('%s.avi', obj.camera_ids{ii}));
                 
-                if ii == length(camera_ids)
-                    cmd = sprintf('start /wait cmd /k %s C:\\Users\\lee\\Documents\\mvelez\\atyson\\runningmouse\\difference_video_2\\main.py -v %s --save-csv -o %s', ...
+                if ii == length(obj.camera_ids)
+                    cmd = sprintf('start /wait cmd /k %s %s -v %s --save-csv -o %s', ...
                         obj.ctl.file.path_config.runningmouse_python_exe, ...
-                        camera_fname, local_dir);
+                        obj.ctl.file.path_config.runningmouse_main_script, ...
+                        camera_fname, obj.local_dir);
                 else
-                    cmd = sprintf('start cmd /k %s C:\\Users\\lee\\Documents\\mvelez\\atyson\\runningmouse\\difference_video_2\\main.py -v %s --save-csv -o %s', ...
+                    cmd = sprintf('start cmd /k %s %s -v %s --save-csv -o %s', ...
                         obj.ctl.file.path_config.runningmouse_python_exe, ...
-                        camera_fname, local_dir);
+                        obj.ctl.file.path_config.runningmouse_main_script, ...
+                        camera_fname, obj.local_dir);
                 end
                 system(cmd);
             end
@@ -65,19 +79,39 @@ classdef CameraProcessingHelper < handle
         
         function transform_avi(obj)
             
-            local_dir = obj.ctl.file.camera_csv_dir_fast(obj.session_id);
-            camera_ids = obj.ctl.get_camera_ids(obj.session_id);
-            
             % run the python pipeline
-            for ii = 1 : length(camera_ids)
+            for ii = 1 : length(obj.camera_ids)
                 
-                camera_fname = fullfile(local_dir, sprintf('%s.avi', camera_ids{ii}));
+                camera_fname = fullfile(obj.local_dir, sprintf('%s.avi', obj.camera_ids{ii}));
                 
                 cmd = sprintf('start /wait cmd /c ffmpeg -i %s %s', camera_fname, strrep(camera_fname, '.avi', '.mp4'));
                 system(cmd);
                 
                 % remove the large .avi
                 delete(camera_fname);
+            end
+        end
+        
+        
+        
+        function save_git(obj)
+            
+            for ii = 1 : length(obj.camera_ids)
+                
+                cfg_fname = fullfile(obj.local_dir, sprintf('%s.cfg', obj.camera_ids{ii}));
+                
+                cmd = sprintf('git -C "%s" rev-parse --show-toplevel', fileparts(obj.ctl.file.path_config.runningmouse_main_script));
+                [~, output] = system(cmd);
+                
+                if ispc
+                    output = strrep(output, '/', '\');
+                end
+                output = output(1:end-1);
+                
+                rm_git = Git(output);
+                rm_git.save(cfg_fname, [], 'runningmouse_git');
+                
+                obj.ctl.save.append_to_git_cfg(cfg_fname, [], 'lib_git');
             end
         end
     end
