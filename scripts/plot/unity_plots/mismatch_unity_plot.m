34e6f26b5ebbe57738dca3f66191513798f091be
lcossell <l.cossell@ucl.ac.uk>
2022-01-05 17:59:47 +0000

    comment mismatch_unity_plot.m

--- scripts/plot/unity_plots/mismatch_unity_plot.m
+++ scripts/plot/unity_plots/mismatch_unity_plot.m
@@ -1,20 +1,59 @@
-%%script for producing a mismatch unity plot
+% Plot mismatch unity plots
+%
+%   Specify options:
+%
+%       experiment_groups:      Will generate unity plots including all
+%                               clusters in the probe recordings 
+%                               in the specified experiment group. e.g. one of:
+%                                   'darkness',
+%                                   'visual_flow',
+%                                   'mismatch_nov20',
+%                                   'mismatch_jul21',
+%                                   'mismatch_darkness_oct21'
+%                               Should be a cell array of strings with each
+%                               entry an experiment group
+%
+%       trial_group_labels:     Will generate a unity plot for each of the
+%                               trials specified. 
+%                               Should be a cell array, with each entry
+%                               either a string specifying a trial group,
+%                               or a cell array of strings specifying
+%                               multiple trial groups.
+%                               e.g. {'RVT_gain_up', 'RV_gain_up'}
+%                               will create two unity plots, the first
+%                               using data from 'RVT_gain_up' trials and
+%                               the second using data from 'RV_gain_up'
+%                               trials. 
+%
+%       marker_type:            the type of marker to use on the plot (any
+%                               of those accepted by matlab) e.g. 'o' or
+%                               'v'. Should be a cell array of the same
+%                               length as `trial_group_labels`.
+%
+%       save_figs:              true or false, whether to save the figures to pdf
+%
+%       overwrite:              true or false. If figure pdf's already exist,
+%                               whether to overwrite 
+%       
+%       figure_dir:             cell array of strings specifying which
+%                               directory to save pdf's. The directory will
+%                               be relative to the directory specified by
+%                               path_config.figure_dir (in
+%                               `path_config.m`), so that {'one', 'two',
+%                               'three'} will save .pdfs to:
+%                               <path_config.figure_dir>\one\two\three\      
+%
+% If `save_figs` is true, one pdf with a unity plot for each of the trial
+% groups specified
+
 
 %%
-% experiment_groups       = {'mismatch_nov20'};
-% trial_group_labels      = {'RVT_gain_up', 'RV_gain_up'};
-% marker_style            = {'o', 'o'};
-
-
-
-%%
-experiment_groups       = {'mismatch_darkness_oct21'};
-trial_group_labels      = {'RT_gain_up'};
-marker_style            = {'o'};
+experiment_groups       = {'mismatch_nov20'};
+trial_group_labels      = {'RVT_gain_up', 'RVT_gain_down'};
+marker_style            = {'o', 'o'};
 save_figs               = true;
 overwrite               = true;
-figure_dir              = {'mismatch_unity_plot', 'mismatch_darkness_oct21'};
-
+figure_dir              = {'mismatch_unity_plot', 'mismatch_nov20'};
 
 
 %%
@@ -32,7 +71,7 @@ direction               = cell(1, length(trial_group_labels));
 for ii = 1 : length(probe_ids)
     
     data        = ctl.load_formatted_data(probe_ids{ii});
-    clusters    = data.selected_clusters;
+    clusters    = data.VISp_clusters;
     
     for jj = 1 : length(trial_group_labels)
         
