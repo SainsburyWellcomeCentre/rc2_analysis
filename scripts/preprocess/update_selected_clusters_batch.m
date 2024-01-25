b8b844c04ed08a6763777937e7952e3ed8a8913d
lcossell <l.cossell@ucl.ac.uk>
2024-01-25 13:33:51 +0000

    Add script to update the analysed clusters if the filtering has been changed@

---
+++ scripts/preprocess/update_selected_clusters_batch.m
@@ -0,0 +1,17 @@
+ctl = RC2Analysis();
+preprocess = RC2Preprocess();
+format = RC2Format();
+
+probe_ids  = ctl.get_probe_ids('visual_flow', ...
+                               'mismatch_nov20', ...
+                               'mismatch_jul21', ...
+                               'mismatch_darkness_oct21', ...
+                               'darkness');
+
+
+for ii = 1 : length(probe_ids)
+    fprintf('Processing %s (%i/%i)\n', probe_ids{ii}, ii, length(probe_ids));
+    %preprocess.create_selected_clusters_txt(probe_ids{ii});
+    format.format(probe_ids{ii});
+end
+    
