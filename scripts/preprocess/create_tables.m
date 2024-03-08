2f4c1c6c2ee91eabedc574913b40903dedbe5e11
lauraporta <ucqflpo@ucl.ac.uk>
2024-03-08 10:49:38 +0000

    Adjust script to recreate tuning curves

--- scripts/preprocess/create_tables.m
+++ scripts/preprocess/create_tables.m
@@ -1,12 +1,16 @@
-experiment_groups    = {'mismatch_darkness_oct21'};%, 'mismatch_nov20', 'mismatch_jul21', };
+experiment_groups    = {'darkness', 'mismatch_darkness_oct21'};%, 'mismatch_nov20', 'mismatch_jul21', };
+trial_group_labels   = {{'RT', 'R', {'T_bank', 'T_RT', 'T_R'}}, {'R', 'T', 'RT_gain_up'}};
 
 ctl                  = RC2Analysis();
-probe_ids            = ctl.get_probe_ids(experiment_groups{:});
 
-for ii = 1 : length(probe_ids)
-    ii
-    % load formatted data
-%     ctl.create_replay_offsets_table(probe_ids{ii});
-%     ctl.create_svm_table(probe_ids{ii});
-    ctl.create_tuning_curves(probe_ids{ii}, {'R', 'T', 'RT_gain_up'});
+for jj = 1 : length(trial_group_labels)
+    probe_ids            = ctl.get_probe_ids(experiment_groups{jj});
+
+    for ii = 1 : length(probe_ids)
+        ii
+        % load formatted data
+    %     ctl.create_replay_offsets_table(probe_ids{ii});
+    %     ctl.create_svm_table(probe_ids{ii});
+        ctl.create_tuning_curves(probe_ids{ii}, trial_group_labels{jj});
+    end
 end
