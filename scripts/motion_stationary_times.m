9cad18a45af95079f600121017af4e4112392a99
lauraporta <ucqflpo@ucl.ac.uk>
2024-07-11 16:58:26 +0100

    Expand measurement of motion and stationary times

--- scripts/motion_stationary_times.m
+++ scripts/motion_stationary_times.m
@@ -3,8 +3,10 @@ probe_ids  = ctl.get_probe_ids('visual_flow', ...
                                'mismatch_nov20', ...
                                'mismatch_jul21', ...
                                'mismatch_darkness_oct21', ...
-                               'darkness');
-trial_types                 = {'RT', 'RT_gain_up', 'R', 'T_bank', 'T_RT', 'T_R', 'T', 'RVT', 'RVT_gain_up','RV', 'RV_gain_up', 'VT_RVT', 'VT_RV', 'V_RVT', 'V_RV'};
+                               'darkness', ...
+                               'passive_same_luminance', ...
+                               'passive_same_luminance_muscimol');
+trial_types                 = {'RT', 'RT_gain_up', 'R', 'T_bank', 'T_RT', 'T_R', 'T', 'RVT', 'RVT_gain_up','RV', 'RV_gain_up', 'VT_RVT', 'VT_RV', 'V_RVT', 'V_RV', 'T_Vstatic', 'V', 'VT'};
 
                            
 stationary_times = [];
@@ -17,7 +19,13 @@ for probe_i = 1 : length(probe_ids)
 
         for trial_i = 1 : length(trials)
             trial  = trials{trial_i}.to_aligned;
-            stationary_times(end+1) = trial.stationary_time;
+            
+            if trial.stationary_time < 3
+                stationary_times(end+1) = 3;
+            else
+                stationary_times(end+1) = trial.stationary_time;
+            end
+
             motion_times(end+1) = trial.motion_time;
         end
     end
@@ -25,14 +33,29 @@ end
 
 
 stationary_times_mean = mean(stationary_times);
+stationary_times_median = median(stationary_times);
 stationary_times_std = std(stationary_times);
 stationary_times_max = max(stationary_times);
 stationary_times_min = min(stationary_times);
+stationary_times_q1 = prctile(stationary_times, 25);
+stationary_times_q3 = prctile(stationary_times, 75);
+
 
 motion_times_mean = mean(motion_times);
+motion_times_median = median(motion_times);
 motion_times_std = std(motion_times);
 motion_times_max = max(motion_times);
 motion_times_min = min(motion_times);
+motion_times_q1 = prctile(motion_times, 25);
+motion_times_q3 = prctile(motion_times, 75);
+
+[p] = signrank(stationary_times, motion_times);
 
 sprintf('Stationary times. Mean: %.2f, std: %.2f, max: %.2f, min: %.2f.', stationary_times_mean, stationary_times_std, stationary_times_max, stationary_times_min)
+sprintf('Median: %.2f, q1: %.2f, q3: %.2f', stationary_times_median, stationary_times_q1, stationary_times_q3)
 sprintf('Motion times. Mean: %.2f, std: %.2f, max: %.2f, min: %.2f.', motion_times_mean, motion_times_std, motion_times_max, motion_times_min)
+sprintf('Median: %.2f, q1: %.2f, q3: %.2f', motion_times_median, motion_times_q1, motion_times_q3)
+sprintf('p: %.5f', p)
+
+
+
