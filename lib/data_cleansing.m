a4edc406431973c582b4e909ae804051b0ead4a6
velezmat <m.velez-fort@ucl.ac.uk>
2025-06-04 14:57:43 +0100

    added all motion clouds experiments so far

--- lib/data_cleansing.m
+++ lib/data_cleansing.m
@@ -147,7 +147,10 @@ if (strcmp(sessions(1).session_id, 'CAA-1121416_rec1_001') && strcmp(sessions(2)
 end
 
 % Include information for passive protocol sequence of motion clouds
-if strcmp(sessions(1).session_id, 'CAA-1123244_rec1_001')
+if strcmp(sessions(1).session_id, 'CAA-1123243_rec1_001') || ...
+        strcmp(sessions(1).session_id, 'CAA-1123244_rec1_001') || ...
+        strcmp(sessions(1).session_id, 'CAA-1123245_rec1_001') || ...
+        strcmp(sessions(1).session_id, 'CAA-1123246_rec1_001')
         
     fname = 'Y:\mvelez\mateoData_rc2\passive_protocol_sequence_motion_clouds.mat';
     passive_protocol_sequence_motion_clouds = load(fname);
